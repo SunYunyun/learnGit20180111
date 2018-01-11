ca2494ad9dc7b7a5a4480d709042828f76dd0bcf
yunyunsun <498640662@qq.com>
2018-01-11 16:22:30 +0800

    first commot

---
+++ scrapylearn/scrapylearn/myconfig.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+UsersConfig = {
+    # 代理
+    'proxy': '',
+
+    # 知乎用户名和密码
+    'email': 'your email',
+    'password': 'your password',
+}
+
+DbConfig = {
+    # db config
+    'user': 'db user',
+    'passwd': 'db password',
+    'db': 'db name',
+    'host': 'db host',
+}
